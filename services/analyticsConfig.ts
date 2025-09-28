dc7dbac58a809ecd560f60f432eab8913ed0b788
mhidper <82749401+mhidper@users.noreply.github.com>
2025-09-28 08:35:51 +0200

    Update analyticsConfig.ts

--- services/analyticsConfig.ts
+++ services/analyticsConfig.ts
@@ -3,7 +3,7 @@
 
 export const GOOGLE_SHEETS_CONFIG = {
   // Pega aquí tu URL de Google Apps Script
-  WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbzQbHoXh7EV_LuBmtEzBetNBnE2ncRTM6-i4E3FYJIs_L2xVQOVbturWLK09eFyrc3goQ/exec',
+  WEBHOOK_URL: 'https://script.google.com/macros/s/AKfycbzyu0dlUInkpTL9cKYProfNXlaAfIjqpGg8Xux7DcAaazDIF5ebpdIlFa4t7O4VbsE0LQ/exec',
   
   // Configuración adicional
   ENABLED: true,
